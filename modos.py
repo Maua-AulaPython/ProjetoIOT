aa72faf980e3463b06ea4f10c449dc36564e8ac1
lrebelo2 <lucasrebelo2@gmail.com>
2015-06-12 11:33:25 -0300

    ajjij

--- modos.py
+++ modos.py
@@ -3,11 +3,12 @@ from mauaserver import db
 class Device(db.Model):
     id= db.Column(db.Integer,primary_key = True)
     nome=db.Column(db.String(20))
-    medidas=db.relationship('Measure')
+    #medidas=db.relationship('Measure')
 
 class Measure(db.Model):
     id=db.Column(db.Integer,primary_key=True)
-    id_device=db.Column(db.Integer,db.ForeignKey('Device.id'))
+    id_device=db.Column(db.Integer,db.ForeignKey('device.id'))
     temperatura=db.Column(db.Float)
     data=db.Column(db.DateTime)
     
+db.create_all();
